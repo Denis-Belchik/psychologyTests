0c2ca65c27da37e661d9a9a8343039eb7c45c7a0
Denis-Belchik <denis1989g@mail.ru>
2023-07-06 15:25:57 +0400

    Init

---
+++ src/main/java/com/setlocal/psychologyTests/PsychologyTestsApplication.java
@@ -0,0 +1,13 @@
+package com.setlocal.psychologyTests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychologyTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsychologyTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/setlocal/psychologyTests/PsychologyTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.setlocal.psychologyTests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychologyTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
