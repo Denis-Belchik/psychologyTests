c0c4045019179282a53f5f16de004dd04486e48e
Setlocal1989 <denis1989g@mail.ru>
2023-07-12 10:58:35 +0400

    До ума довел точно

--- src/main/java/com/setlocal/psychologyTests/controller/QuestionController.java
+++ src/main/java/com/setlocal/psychologyTests/controller/QuestionController.java
@@ -8,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Arrays;
+
 @Controller
 public class QuestionController {
 
